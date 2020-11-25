b3fcdbadc2022466bccfe2c52ae2a75c57fa4406
Vance Longwill <vancelongwill@gmail.com>
2020-11-25 10:48:08 +0000

    Add convenience methods for common LIST operations

--- client.go
+++ client.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"fmt"
 
 	"io"
@@ -226,14 +227,17 @@ type QueryDescription struct {
 	Topology string `json:"topology"`
 }
 
-// ExecResult is the response result from the /ksql endpoint
-type ExecResult struct {
-	// Common to all responses
-
+// Common to all exec responses
+type commonResult struct {
 	// StatementText is the text of the SQL statement where the error occurred
 	StatementText string `json:"statementText"`
 	// A list of non-fatal warning messages
 	Warnings []Warning `json:"warnings"`
+}
+
+// ExecResult is the response result from the /ksql endpoint
+type ExecResult struct {
+	commonResult
 
 	// CREATE, DROP, TERMINATE
 
@@ -303,6 +307,74 @@ func (c *Client) Exec(ctx context.Context, payload ExecPayload) ([]ExecResult, e
 		results = append(results, result)
 	}
 	return results, nil
+}
+
+func (c *Client) doListExec(ctx context.Context, payload ExecPayload) (ExecResult, error) {
+	var resp ExecResult
+	results, err := c.Exec(ctx, payload)
+	if err != nil {
+		return resp, err
+	}
+	if len(results) == 0 {
+		return resp, errors.New("Unexpected empty response list from Exec")
+	}
+	return results[0], nil
+}
+
+// ListStreamsResult represents the API response from the `LIST STREAMS;` operation
+type ListStreamsResult struct {
+	commonResult
+	// Streams is the list of streams returned
+	Streams []Stream `json:"streams,omitempty"`
+}
+
+// ListStreams is a convenience method which executes a `LIST STREAMS;` operation
+func (c *Client) ListStreams(ctx context.Context) (*ListStreamsResult, error) {
+	res, err := c.doListExec(ctx, ExecPayload{KSQL: "LIST STREAMS;"})
+	if err != nil {
+		return nil, err
+	}
+	return &ListStreamsResult{
+		Streams: res.Streams,
+	}, nil
+}
+
+// ListTablesResult represents the API response from the `LIST TABLES;` operation
+type ListTablesResult struct {
+	commonResult
+	// Tables is the list of tables returned
+	Tables []Table `json:"tables,omitempty"`
+}
+
+// ListTables is a convenience method which executes a `LIST TABLES;` operation
+func (c *Client) ListTables(ctx context.Context) (*ListTablesResult, error) {
+	res, err := c.doListExec(ctx, ExecPayload{KSQL: "LIST TABLES;"})
+	if err != nil {
+		return nil, err
+	}
+	return &ListTablesResult{
+		commonResult: res.commonResult,
+		Tables:       res.Tables,
+	}, nil
+}
+
+// ListQueriesResult represents the API response from the `LIST QUERIES;` operation
+type ListQueriesResult struct {
+	commonResult
+	// Queries is the list of running queries
+	Queries []Query `json:"tables,omitempty"`
+}
+
+// ListQueries is a convenience method which executes a `LIST QUERIES;` operation
+func (c *Client) ListQueries(ctx context.Context) (*ListQueriesResult, error) {
+	res, err := c.doListExec(ctx, ExecPayload{KSQL: "LIST QUERIES;"})
+	if err != nil {
+		return nil, err
+	}
+	return &ListQueriesResult{
+		commonResult: res.commonResult,
+		Queries:      res.Queries,
+	}, nil
 }
 
 // QueryResultHeader is a header object which contains details of the push & pull query results
